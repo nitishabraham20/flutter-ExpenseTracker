e2666933e375c0bedb4d3ebcd889c343aa82a48c
Nitish Abraham Cherian <nitishcherian@gmail.com>
2024-04-04 23:37:14 +0530

    initial commit

---
+++ lib/models/expense.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
+import 'package:intl/intl.dart';
+
+final formatter = DateFormat.yMd();
+
+const uuid = Uuid();
+
+enum Category { food, travel, leisure, work }
+
+const categoryIcons = {
+  Category.food: Icons.lunch_dining,
+  Category.travel: Icons.flight_takeoff,
+  Category.leisure: Icons.movie,
+  Category.work: Icons.work,
+};
+
+class Expense {
+  Expense({
+    required this.title,
+    required this.amount,
+    required this.date,
+    required this.category,
+  }) : id = uuid.v4();
+
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+  final Category category;
+
+  String get formattedDate {
+    return formatter.format(date);
+  }
+
+  IconData get categoryIcon {
+    switch (category) {
+      case Category.food:
+        return Icons.lunch_dining;
+      case Category.travel:
+        return Icons.flight_takeoff;
+      case Category.leisure:
+        return Icons.movie;
+      case Category.work:
+        return Icons.work;
+    }
+  }
+}
---
+++ lib/models/expense_bucket.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:expense_tracker/models/expense.dart';
+
+class ExpenseBucket {
+  const ExpenseBucket({
+    required this.category,
+    required this.expenses,
+  });
+
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
+      : expenses = allExpenses
+            .where((expense) => expense.category == category)
+            .toList();
+
+  final Category category;
+  final List<Expense> expenses;
+
+  double get totalExpenses {
+    double sum = 0;
+
+    for (final expense in expenses) {
+      sum += expense.amount; // sum = sum + expense.amount
+    }
+
+    return sum;
+  }
+
+  IconData get categoryIcon {
+    switch (category) {
+      case Category.food:
+        return Icons.lunch_dining;
+      case Category.travel:
+        return Icons.flight_takeoff;
+      case Category.leisure:
+        return Icons.movie;
+      case Category.work:
+        return Icons.work;
+    }
+  }
+}
